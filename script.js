73bec18e431379f1aa3588955320137598cf4b5e
muendokennedy <kennedymunyao999@gmail.com>
2023-06-06 11:45:57 +0300

    Finished the frontend design of the login page

---
+++ script.js
@@ -0,0 +1,22 @@
+const passwordField1 = document.getElementById("pwd");
+const passwordField2 = document.getElementById("confirm-pwd");
+const showBtn = document.querySelector(".input-box .show-btn");
+
+passwordField1.onkeyup = () => {
+  if(passwordField1.value){
+  showBtn.style.display = "block";
+  showBtn.onclick = () => {
+    if(passwordField1.type == "password" || passwordField2.type == "password"){
+      passwordField1.type = "text";
+      passwordField2.type = "text";
+      showBtn.innerText = "HIDE";
+    } else {
+      passwordField1.type = "password";
+      passwordField2.type = "password";
+      showBtn.innerText = "SHOW";
+      }
+    }
+  }else if(!passwordField1.value){
+    showBtn.style.display = "none";
+  }
+}
